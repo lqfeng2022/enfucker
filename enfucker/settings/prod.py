d8124769ea4c9886163cd2d22d13fe91ecfcdd62
lqfeng2022 <simonlqf@hotmail.com>
2026-01-07 17:05:16 +0800

    add api keys

--- enfucker/settings/prod.py
+++ enfucker/settings/prod.py
@@ -9,13 +9,19 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+DEEPSEEK_BASE_URL = 'https://api.deepseek.com'
+
+DEEPSEEK_API_KEY = os.environ.get('DEEPSEEK_API_KEY')
+ELEVENLABS_API_KEY = os.environ.get('ELEVENLABS_API_KEY')
+
+
 # Specify the server and server can run this application
 # It’s only required if debug turned off
 ALLOWED_HOSTS = [
     'clipwords.me',
     'www.clipwords.me',
 ]
-
-DATABASES = {
-    'default': dj_database_url.config()
-}
