cca0bf3b2197c9cba2a3032499dc4534d3ed6088
lqfeng2022 <simonlqf@hotmail.com>
2026-01-14 12:12:19 +0800

    fix admin underline link issue

--- enfucker/settings/common.py
+++ enfucker/settings/common.py
@@ -236,3 +236,20 @@ CACHES = {
         'TIMEOUT': None,  # optional
     }
 }
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],  # required
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
